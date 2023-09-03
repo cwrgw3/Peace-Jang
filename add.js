440adf40207d9effbf31518632974b604bb0f84b
kim min ho <128780649+cwrgw3@users.noreply.github.com>
2023-09-04 02:04:15 +0900

    Add files via upload

--- add.js
+++ add.js
@@ -2,18 +2,6 @@ function SideMenuOpen() {
     alert('되나되나되나 호오오우 된다');
 }
 
-function showSideMenu() {
-    var SideMenu = document.getElementById("SideMenu");
-    // 도형을 보이게 하려면 left 속성을 0으로 변경합니다.
-    SideMenu.style.right = "0vw";
-}
-
-function hideSideMenu() {
-    var SideMenu = document.getElementById("SideMenu");
-    // 도형을 다시 화면 왼쪽 밖으로 이동하도록 설정합니다.
-    SideMenu.style.left = "-100vw";
-}
-
 
 function toggleSideMenu() {
     var SideMenu = document.getElementById("SideMenu");
@@ -22,9 +10,24 @@ function toggleSideMenu() {
     if (SideMenu.classList.contains("show")) {
         SideMenu.classList.remove("show");
         SideMenu.classList.add("hide");
-        
+
     } else {
         SideMenu.classList.remove("hide");
         SideMenu.classList.add("show");
     }
 }
+
+
+
+function toggleDropdown(button) {
+    var dropdownContent = button.nextElementSibling;
+    dropdownContent.classList.toggle("active");
+}
+
+// 모든 드롭다운 버튼에 클릭 이벤트 핸들러를 등록합니다.
+var buttons = document.querySelectorAll(".dropdown-button");
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        toggleDropdown(button);
+    });
+});
